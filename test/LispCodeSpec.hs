52b6ec0292b22d046f4b20712d43891592bba6f3
hyone <elliptical.galaxy@gmail.com>
2012-12-31 18:24:38 +0900

    Convert unit test to hspec.

---
+++ test/LispCodeSpec.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module LispCodeSpec where
+
+import Control.Monad.IO.Class (liftIO)
+import Test.Hspec
+import Scheme.Eval (initEnv, evalString)
+
+
+spec :: Spec
+spec = do
+  describe "arithmetic" $ do
+    it "additional" $ do
+      env <- initEnv
+      evalString env "(+ 1 2 3)" `shouldReturn` "6"
+      evalString env "(+ 4 5 6)" `shouldReturn` "15"
+      -- evalString env "(+)"       `shouldReturn` "0"
+      
+    it "multiplication" $ do
+      env <- initEnv
+      evalString env "(* 1 2 3)" `shouldReturn` "6"
+      evalString env "(* 4 5 6)" `shouldReturn` "120"
+      -- evalString env "(*)"       `shouldReturn` "0"
+
+  describe "list" $ do
+    it "basic" $ do
+      env <- initEnv
+      evalString env "(cons 1 '(2 3))" `shouldReturn` "(1 2 3)"
+      evalString env "(car '(1 2 3))"  `shouldReturn` "1"
+      evalString env "(cdr '(1 2 3))"  `shouldReturn` "(2 3)"
+
+  describe "function" $ do
+    it "basic" $ do
+      env <- initEnv
+      _   <- evalString env "(define (f x y) (+ x y))"
+      evalString env "(f 1 2)" `shouldReturn` "3"
+      evalString env "(f 5)"   `shouldReturn` "Expected 2 args; found values 5"
+
+    it "recursive function" $ do
+      env <- initEnv
+      _   <- evalString env "(define (fact x) (if (= x 1) 1 (* x (fact (- x 1)))))"
+      evalString env "(fact 10)" `shouldReturn` "3628800"
+
+    it "closure" $ do
+      env <- initEnv
+      _   <- evalString env "(define (gen-accumulator n) (lambda (i) (set! n (+ n i))))"
+      _   <- evalString env "(define acc (gen-accumulator 5))"
+      evalString env "(acc 3)" `shouldReturn` "8"
+      evalString env "(acc 5)" `shouldReturn` "13"
+      evalString env "(acc 6)" `shouldReturn` "19"
+
+
+main :: IO ()
+main = hspec spec
