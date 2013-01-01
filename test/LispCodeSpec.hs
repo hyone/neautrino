5ca5d01a1ae8f9de41f7f492a903870a2dc98f68
hyone <elliptical.galaxy@gmail.com>
2013-01-02 00:34:23 +0900

    Add title group to specs in LispCodeSpec.hs.

--- test/LispCodeSpec.hs
+++ test/LispCodeSpec.hs
@@ -6,53 +6,54 @@ import Scheme.Eval (initEnv, evalString)
 
 specs :: Spec
 specs = do
-  describe "arithmetic" $ do
-    it "additional" $ do
-      env <- initEnv
-      evalString env "(+ 1 2 3)" `shouldReturn` "6"
-      evalString env "(+ 4 5 6)" `shouldReturn` "15"
-      -- evalString env "(+)"       `shouldReturn` "0"
-      
-    it "multiplication" $ do
-      env <- initEnv
-      evalString env "(* 1 2 3)" `shouldReturn` "6"
-      evalString env "(* 4 5 6)" `shouldReturn` "120"
-      -- evalString env "(*)"       `shouldReturn` "0"
+  describe "LispCode" $ do
+    describe "arithmetic" $ do
+      it "additional" $ do
+        env <- initEnv
+        evalString env "(+ 1 2 3)" `shouldReturn` "6"
+        evalString env "(+ 4 5 6)" `shouldReturn` "15"
+        -- evalString env "(+)"       `shouldReturn` "0"
 
-  describe "list" $ do
-    it "basic" $ do
-      env <- initEnv
-      evalString env "(cons 1 '(2 3))" `shouldReturn` "(1 2 3)"
-      evalString env "(car '(1 2 3))"  `shouldReturn` "1"
-      evalString env "(cdr '(1 2 3))"  `shouldReturn` "(2 3)"
+      it "multiplication" $ do
+        env <- initEnv
+        evalString env "(* 1 2 3)" `shouldReturn` "6"
+        evalString env "(* 4 5 6)" `shouldReturn` "120"
+        -- evalString env "(*)"       `shouldReturn` "0"
 
-  describe "variable" $ do
-    it "basic" $ do
-      env <- initEnv
-      _   <- evalString env "(define a 3)"
-      evalString env "a" `shouldReturn` "3"
-      _   <- evalString env "(set! a #\\c)"
-      evalString env "a" `shouldReturn` "#\\c"
+    describe "list" $ do
+      it "basic" $ do
+        env <- initEnv
+        evalString env "(cons 1 '(2 3))" `shouldReturn` "(1 2 3)"
+        evalString env "(car '(1 2 3))"  `shouldReturn` "1"
+        evalString env "(cdr '(1 2 3))"  `shouldReturn` "(2 3)"
 
-  describe "function" $ do
-    it "basic" $ do
-      env <- initEnv
-      _   <- evalString env "(define (f x y) (+ x y))"
-      evalString env "(f 1 2)" `shouldReturn` "3"
-      evalString env "(f 5)"   `shouldReturn` "Expected 2 args; found values 5"
+    describe "variable" $ do
+      it "basic" $ do
+        env <- initEnv
+        _   <- evalString env "(define a 3)"
+        evalString env "a" `shouldReturn` "3"
+        _   <- evalString env "(set! a #\\c)"
+        evalString env "a" `shouldReturn` "#\\c"
 
-    it "recursive function" $ do
-      env <- initEnv
-      _   <- evalString env "(define (fact x) (if (= x 1) 1 (* x (fact (- x 1)))))"
-      evalString env "(fact 10)" `shouldReturn` "3628800"
+    describe "function" $ do
+      it "basic" $ do
+        env <- initEnv
+        _   <- evalString env "(define (f x y) (+ x y))"
+        evalString env "(f 1 2)" `shouldReturn` "3"
+        evalString env "(f 5)"   `shouldReturn` "Expected 2 args; found values 5"
 
-    it "closure" $ do
-      env <- initEnv
-      _   <- evalString env "(define (gen-accumulator n) (lambda (i) (set! n (+ n i))))"
-      _   <- evalString env "(define acc (gen-accumulator 5))"
-      evalString env "(acc 3)" `shouldReturn` "8"
-      evalString env "(acc 5)" `shouldReturn` "13"
-      evalString env "(acc 6)" `shouldReturn` "19"
+      it "recursive function" $ do
+        env <- initEnv
+        _   <- evalString env "(define (fact x) (if (= x 1) 1 (* x (fact (- x 1)))))"
+        evalString env "(fact 10)" `shouldReturn` "3628800"
+
+      it "closure" $ do
+        env <- initEnv
+        _   <- evalString env "(define (gen-accumulator n) (lambda (i) (set! n (+ n i))))"
+        _   <- evalString env "(define acc (gen-accumulator 5))"
+        evalString env "(acc 3)" `shouldReturn` "8"
+        evalString env "(acc 5)" `shouldReturn` "13"
+        evalString env "(acc 6)" `shouldReturn` "19"
 
 
 main :: IO ()
