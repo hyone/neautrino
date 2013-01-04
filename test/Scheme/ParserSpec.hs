e14332d7a176247e2279c0a97797689cd260269a
hyone <elliptical.galaxy@gmail.com>
2013-01-04 13:51:27 +0900

    Add Scheme.TestUtil

--- test/Scheme/ParserSpec.hs
+++ test/Scheme/ParserSpec.hs
@@ -1,138 +1,130 @@
 module Scheme.ParserSpec (spec) where
 
 import Test.Hspec
-import Test.HUnit (assertFailure)
+import Scheme.TestUtil (shouldBeT, shouldErrorT)
 import Data.Array (listArray)
 
 import Scheme.Parser
+import Scheme.Error (LispError(..))
 import Scheme.Type (LispVal(..))
 import Scheme.Error (ThrowsError)
 
 
-message :: String -> String -> String
-message expect got = "expected: " ++ expect ++ "\n but got: " ++ got
-
-shouldParse :: (Eq a, Show a) => ThrowsError a -> a -> Expectation
-shouldParse (Right x) expect = x `shouldBe` expect
-shouldParse err       expect = assertFailure $ message (show expect) (show err)
-
-assertParseError :: (Eq a, Show a) => ThrowsError a -> Expectation
-assertParseError (Left _) = return ()
-assertParseError x        = assertFailure $ message "ParseError" (show x)
-
+parserError :: LispError
+parserError = ParserError undefined
 
 spec :: Spec
 spec = do
   describe "Scheme.Parser" $ do
     describe "bool" $ do
       it "should parse: #t" $
-        readExpr "#t" `shouldParse` Bool True
+        readExpr "#t" `shouldBeT` Bool True
 
       it "should parse: #f" $
-        readExpr "#f" `shouldParse` Bool False
+        readExpr "#f" `shouldBeT` Bool False
 
       it "should parse error: #true" $
-        assertParseError $ readExpr "#\\true" 
+        readExpr "#\\true" `shouldErrorT` parserError
 
       it "should parse error: #false" $
-        assertParseError $ readExpr "#\\false" 
+        readExpr "#\\false" `shouldErrorT` parserError
 
     describe "char" $ do
       it "should parse: #\\c" $ 
-        readExpr "#\\c" `shouldParse` Character 'c'
+        readExpr "#\\c" `shouldBeT` Character 'c'
 
       it "should parse: #\\space" $ 
-        readExpr "#\\space" `shouldParse` Character ' '
+        readExpr "#\\space" `shouldBeT` Character ' '
 
       it "should parse: #\\newline" $ 
-        readExpr "#\\newline" `shouldParse` Character '\n'
+        readExpr "#\\newline" `shouldBeT` Character '\n'
 
       it "should parse error: #\\ss" $ 
-        assertParseError $ readExpr "#\\ss" 
+        readExpr "#\\ss" `shouldErrorT` parserError
 
     describe "string" $ do
       it "should parse: \"hello world\"" $ 
-        readExpr "\"hello world\"" `shouldParse` String "hello world"
+        readExpr "\"hello world\"" `shouldBeT` String "hello world"
 
       it "should parse: \"\"" $ 
-        readExpr "\"\"" `shouldParse` String ""
+        readExpr "\"\"" `shouldBeT` String ""
           
     describe "number" $ do
       it "should parse: 2" $ 
-        readExpr "2" `shouldParse` Number 2
+        readExpr "2" `shouldBeT` Number 2
 
       it "should parse: #b110" $ 
-        readExpr "#b110" `shouldParse` Number 6
+        readExpr "#b110" `shouldBeT` Number 6
 
       it "should parse: #o37" $ 
-        readExpr "#o37" `shouldParse` Number 31
+        readExpr "#o37" `shouldBeT` Number 31
 
       it "should parse: #d37" $ 
-        readExpr "#d37" `shouldParse` Number 37
+        readExpr "#d37" `shouldBeT` Number 37
 
       it "should parse: #xA7" $ 
         pending "a bug not to accept 'a' - 'f' characters."
-        -- readExpr "#xA7"  `shouldParse` Number 167
+        -- readExpr "#xA7"  `shouldBeT` Number 167
 
     describe "float" $ do
       it "should parse: 3.24" $ 
-        readExpr "3.24" `shouldParse` Float 3.24
+        readExpr "3.24" `shouldBeT` Float 3.24
 
       it "should parse: 3.2e3" $
-        readExpr "3.24e3" `shouldParse` Float 3240.0
+        readExpr "3.24e3" `shouldBeT` Float 3240.0
 
     describe "list" $ do
       it "should parse: (hoge)" $ 
-        readExpr "(hoge)" `shouldParse`
-          List [(Atom "hoge")]
+        readExpr "(hoge)" `shouldBeT`
+          List [Atom "hoge"]
 
       it "should parse: (+ 1 2 3)" $ 
-        readExpr "(+ 1 2 3)" `shouldParse`
-          List [(Atom "+"), (Number 1), (Number 2), (Number 3)]
+        readExpr "(+ 1 2 3)" `shouldBeT`
+          List [Atom "+", Number 1, Number 2, Number 3]
 
       it "should parse: ()" $ 
-        readExpr "()" `shouldParse` List []
+        readExpr "()" `shouldBeT` List []
 
     describe "pairs" $ do
       it "should parse: (a . 2)" $
-        readExpr "(a . 2)" `shouldParse`
-          DottedList [(Atom "a")] (Number 2)
+        readExpr "(a . 2)" `shouldBeT`
+          DottedList [Atom "a"] (Number 2)
 
       it "should parse: (a #\\c . 2)" $ 
-        readExpr "(a #\\c . 2)" `shouldParse`
-          DottedList [(Atom "a"), (Character 'c')] (Number 2)
+        readExpr "(a #\\c . 2)" `shouldBeT`
+          DottedList [Atom "a", Character 'c'] (Number 2)
 
       it "should parse error: (.)" $ 
-        assertParseError $ readExpr "(.)"
+        readExpr "(.)" `shouldErrorT` parserError
 
     describe "vector" $ do
       it "should parse: #(1 2 3)" $ 
-        readExpr "#(1 2 3)" `shouldParse`
-          Vector (listArray (0, 2) [(Number 1), (Number 2), (Number 3)])
+        readExpr "#(1 2 3)" `shouldBeT`
+          Vector (listArray (0, 2) [Number 1, Number 2, Number 3])
 
       it "should parse: #()" $ 
-        readExpr "#()" `shouldParse`
+        readExpr "#()" `shouldBeT`
           Vector (listArray (0, -1) [])
 
     describe "quote" $ do
       it "should parse: '(1 2 3)" $
-        readExpr "'(1 2 3)" `shouldParse`
+        readExpr "'(1 2 3)" `shouldBeT`
           List [Atom "quote", List [Number 1, Number 2, Number 3]]
 
       it "should parse: `(1 2 ,a 4)" $
-        readExpr "`(1 2 ,a 4)" `shouldParse`
+        readExpr "`(1 2 ,a 4)" `shouldBeT`
           List [Atom "quasiquote", List [Number 1, Number 2, List [Atom "unquote", Atom "a"], Number 4]]
 
     describe "comment" $ do
       it "should parse 2 ; comment" $
-         readExpr "2 ; comment"   `shouldParse` Number 2
+         readExpr "2 ; comment"   `shouldBeT` Number 2
       it "should parse ; comment\n 2" $
-         readExpr "; comment\n 2" `shouldParse` Number 2
+         readExpr "; comment\n 2" `shouldBeT` Number 2
 
 
     describe "pragmatic case" $ do
       it "shoud parse: (define (map func lst)(foldr (lambda (x acc) (cons (func x) acc)) '() lst))" $
-        readExpr "(define (map func lst) (foldr (lambda (x acc) (cons (func x) acc)) '() lst))" `shouldParse`
+        readExpr "(define (map func lst) (foldr (lambda (x acc) (cons (func x) acc)) '() lst))" `shouldBeT`
           List [ Atom "define"
                , List [ Atom "map", Atom "func", Atom "lst"]
                , List [ Atom "foldr"
---
+++ test/Scheme/TestUtil.hs
@@ -0,0 +1,74 @@
+module Scheme.TestUtil (
+  evalAST
+, shouldContain    
+, shouldEitherT
+, shouldBeT
+, shouldReturnT
+, shouldErrorT
+, shouldErrorReturnT
+) where
+
+import Test.Hspec
+import Test.HUnit
+import Control.Monad.Error (runErrorT)
+import Data.List (isInfixOf)
+
+import Scheme.Error (LispError(..), ThrowsError)
+import Scheme.Env (Env)
+import Scheme.Type (LispVal)
+import Scheme.Eval (eval)
+
+
+newtype ErrorType = ErrorType { getErrorHolder ::  LispError }
+
+instance Eq ErrorType where
+  (ErrorType (NumArgsError x _))        == (ErrorType (NumArgsError y _))        = x == y
+  (ErrorType (TypeMismatchError x _))   == (ErrorType (TypeMismatchError y _))   = x == y
+  (ErrorType (ParserError _))           == (ErrorType (ParserError _))           = True
+  (ErrorType (SyntaxError x _))         == (ErrorType (SyntaxError y _))         = x == y
+  (ErrorType (BadSpecialFormError x _)) == (ErrorType (BadSpecialFormError y _)) = x == y
+  (ErrorType (NotFunctionError x _))    == (ErrorType (NotFunctionError y _))    = x == y
+  (ErrorType (UnboundVarError x _))     == (ErrorType (UnboundVarError y _))     = x == y
+  (ErrorType (DefaultError _))          == (ErrorType (DefaultError _))          = True
+  _                                     == _                                     = False
+
+instance Show ErrorType where
+  show (ErrorType (NumArgsError x _))        = "NumArgsError " ++ show x
+  show (ErrorType (TypeMismatchError x _))   = "TypeMismatchError " ++ x
+  show (ErrorType (ParserError _))           = "ParseError"
+  show (ErrorType (SyntaxError x _))         = "SyntaxError " ++ x
+  show (ErrorType (BadSpecialFormError x _)) = "BadSpecialFormError " ++ x
+  show (ErrorType (NotFunctionError x _))    = "NotFunctionError " ++ x
+  show (ErrorType (UnboundVarError x _))     = "UnboundVarError " ++ x
+  show (ErrorType (DefaultError _))          = "DefaultError"
+
+
+evalAST :: Env -> LispVal -> IO (ThrowsError LispVal)
+evalAST env = runErrorT . eval env
+
+
+message :: String -> String -> String
+message expected got = "expected: " ++ expected ++ "\n but got: " ++ got
+
+shouldContain :: String -> String -> Expectation
+shouldContain got expected = assertBool (message (show expected) (show got))
+                                        (expected `isInfixOf` got)
+
+shouldEitherT :: (Eq a, Show a) => ThrowsError a -> ThrowsError a -> Expectation
+shouldEitherT (Right x)  (Right expected) = x `shouldBe` expected
+shouldEitherT (Left err) (Left expected)  = ErrorType err `shouldBe` ErrorType expected
+shouldEitherT err        expected         = assertFailure $ message (show expected) (show err)
+
+shouldBeT :: (Eq a, Show a) => ThrowsError a -> a -> Expectation
+shouldBeT got@(Right _) expected = got `shouldEitherT` Right expected
+shouldBeT err           expected = assertFailure $ message (show expected) (show err)
+
+shouldReturnT :: (Eq a, Show a) => IO (ThrowsError a) -> a -> Expectation
+shouldReturnT action expected = action >>= (`shouldBeT` expected)
+  
+shouldErrorT :: (Eq a, Show a) => ThrowsError a -> LispError -> Expectation
+shouldErrorT got@(Left _) expected = got `shouldEitherT` Left expected
+shouldErrorT x            expected = assertFailure $ message (show $ ErrorType expected) (show x)
+
+shouldErrorReturnT :: (Eq a, Show a) => IO (ThrowsError a) -> LispError -> Expectation
+shouldErrorReturnT action expected = action >>= (`shouldErrorT` expected)
