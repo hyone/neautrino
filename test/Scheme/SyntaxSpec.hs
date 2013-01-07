a9b41e8d866d5b7e7030b231d31ae45a58ff0998
hyone <elliptical.galaxy@gmail.com>
2013-01-07 14:17:02 +0900

    Rewrite code to build AST by using quasiquoter to improve readabilty

--- test/Scheme/SyntaxSpec.hs
+++ test/Scheme/SyntaxSpec.hs
@@ -1,4 +1,5 @@
-{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Scheme.SyntaxSpec (spec) where
 
 import Test.Hspec
@@ -6,6 +7,7 @@ import Scheme.TestUtil (evalAST, shouldReturnT)
 
 import Scheme.Eval (initEnv)
 import Scheme.Type (LispVal(..))
+import Scheme.TH (parseScheme)
 
   
 spec :: Spec
@@ -14,27 +16,24 @@ spec =
     describe "begin" $ do
       it "evaluates multi expressions sequentially." $ do
         env <- initEnv
-        evalAST env (List [ Atom "begin"
-                          , List [Atom "define", Atom "x", Integer 5]
-                          , List [Atom "set!", Atom "x", Integer 9]
-                          , Atom "x"])
-          `shouldReturnT` Integer 9
+        evalAST env [parseScheme|
+          (begin (define x 5) (set! x 9) x)
+        |] `shouldReturnT` Integer 9
 
     describe "let" $ do
       it "should treat lexical bindings: (let ((a 3) (b 5)) a (+ a b))" $ do
         env <- initEnv
-        evalAST env (List [ Atom "define", Atom "a", Integer 1 ])
-        evalAST env (List [ Atom "let"
-                          , List [ List [Atom "a", Integer 3]
-                                 , List [Atom "b", Integer 5]]
-                          , Atom "a"
-                          , List [ Atom "+", Atom "a", Atom "b" ]])
-          `shouldReturnT` Integer 8
+        _   <- evalAST env [parseScheme|
+          (define a 1)
+        |]
+        evalAST env [parseScheme|
+          (let ((a 3) (b 5)) a (+ a b))
+        |] `shouldReturnT` Integer 8
         evalAST env (Atom "a") `shouldReturnT` Integer 1
 
       it "should return #undef at: (let ())" $ do
         env <- initEnv
-        evalAST env (List [ Atom "let", List []])
+        evalAST env [parseScheme| (let ()) |]
           `shouldReturnT` Undefined
 
 
